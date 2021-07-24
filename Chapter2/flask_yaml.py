5316e88e6e8c642b00196b6f94b3fa1cd16b78bf
kdh_donggu <kdh.candoanything@thecommerce.co.kr>
2021-07-24 22:18:14 +0900

    flask 기본

---
+++ Chapter2/flask_yaml.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+import yaml
+app = Flask(__name__)
+
+
+def yamlify(data, status=200, headers=None):
+    _headers = {"Content-Type" : "application/x-yaml"}
+    if headers is not None:
+        _headers.update(headers)
+    return yaml.safe_dump(data), status, _headers
+
+@app.route("/api")
+def my_microservice():
+    return yamlify(["hello", "yaml", "world"])
+
+
+if __name__ == "__main__":
+    app.run()
